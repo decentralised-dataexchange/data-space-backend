d90d10dc06f65a73e36f758b247f17053a56e9f6
Albin Antony <albin.pa3@gmail.com>
2024-03-25 16:46:11 +0530

    Fix: #3 Add jwt authentication

--- dataspace_backend/settings.py
+++ dataspace_backend/settings.py
@@ -137,8 +137,7 @@ REST_AUTH_SERIALIZERS = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
