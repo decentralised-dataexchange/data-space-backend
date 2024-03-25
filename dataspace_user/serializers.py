899eaaa61c2807c37a22bf409e9dbabda803e3b0
Albin Antony <albin.pa3@gmail.com>
2024-03-25 16:46:11 +0530

    Fix: #3 Add jwt authentication

--- dataspace_user/serializers.py
+++ dataspace_user/serializers.py
@@ -1,13 +1,19 @@
 from .models import DataspaceUser
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
+UserModel = get_user_model()
 
 class RegisterDataspaceUserSerializer(serializers.ModelSerializer):
-
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = DataspaceUser
         fields = ['id', 'email', 'password']
+    
+    def create(self, validated_data):
+        user = UserModel.objects.create_user(**validated_data)  # Use create_user method to handle password hashing
+        return user
 
 class DataspaceUserSerializer(serializers.ModelSerializer):
 
