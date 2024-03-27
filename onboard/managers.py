8010c8f079c30e4abc24ab642c5f575d9cf954c6
Albin Antony <albin.pa3@gmail.com>
2024-03-27 13:33:10 +0530

    Fix: #20 Implementation for data source admin

--- onboard/managers.py
+++ onboard/managers.py
@@ -12,7 +12,7 @@ class DataspaceUserManager(BaseUserManager):
         Create and save a User with the given email and password.
         """
         if not email:
-            raise ValueError(_("The Email must be set"))
+            raise ValueError(("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
