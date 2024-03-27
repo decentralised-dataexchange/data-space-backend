5c53fb3bf98c884357017ac7bce0175a606698c5
Albin Antony <albin.pa3@gmail.com>
2024-03-27 13:33:10 +0530

    Fix: #20 Implementation for data source admin

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DataSourceView, DataSourceCoverImageView, DataSourceLogoImageView
+from .views import DataSourceView, DataSourceCoverImageView, DataSourceLogoImageView, AdminView
 
 urlpatterns = [
     path("data-source/", DataSourceView.as_view(), name="data_source"),
@@ -7,4 +7,6 @@ urlpatterns = [
          DataSourceCoverImageView.as_view(), name="cover_image"),
     path("data-source/logoimage/",
          DataSourceLogoImageView.as_view(), name="logo_image"),
+    path("admin/",
+         AdminView.as_view(), name="admin"),
 ]
--- onboard/urls.py
+++ onboard/urls.py
@@ -9,7 +9,6 @@ from .views import CreateUserView, UserDetail
 urlpatterns = [
     path("extras/", include("rest_auth.urls")),
     path("register/", csrf_exempt(CreateUserView.as_view())),
-    path("admin/", csrf_exempt(UserDetail.as_view())),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("password/reset/", csrf_exempt(PasswordChangeView.as_view()), name="password_reset")
