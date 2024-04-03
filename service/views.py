ffabb944826d51358500d97ecbe4d6d3478be776
Albin Antony <albin.pa3@gmail.com>
2024-04-04 00:20:04 +0530

    Fix: #42 Update data disclosure agreement model

--- service/views.py
+++ service/views.py
@@ -19,15 +19,19 @@ class DataSourceCoverImageView(View):
             # Get the DataSource instance
             datasource = DataSource.objects.get(pk=dataSourceId)
         except DataSource.DoesNotExist:
-            return JsonResponse({'error': 'Data source not found'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Data source not found"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         try:
             image = ImageModel.objects.get(pk=datasource.coverImageId)
         except ImageModel.DoesNotExist:
-            return JsonResponse({'error': 'Cover image not found'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Cover image not found"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         # Return the binary image data as the HTTP response
-        return HttpResponse(image.image_data, content_type='image/jpeg')
+        return HttpResponse(image.image_data, content_type="image/jpeg")
 
 
 class DataSourceLogoImageView(View):
@@ -37,67 +41,84 @@ class DataSourceLogoImageView(View):
             # Get the DataSource instance
             datasource = DataSource.objects.get(pk=dataSourceId)
         except DataSource.DoesNotExist:
-            return JsonResponse({'error': 'Data source not found'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Data source not found"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         try:
             image = ImageModel.objects.get(pk=datasource.logoId)
         except ImageModel.DoesNotExist:
-            return JsonResponse({'error': 'Logo image not found'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"error": "Logo image not found"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         # Return the binary image data as the HTTP response
-        return HttpResponse(image.image_data, content_type='image/jpeg')
+        return HttpResponse(image.image_data, content_type="image/jpeg")
 
 
 class DataSourcesView(View):
     def get(self, request):
-        dataSourceId_param = request.GET.get('dataSourceId')
+        dataSourceId_param = request.GET.get("dataSourceId")
+
         if dataSourceId_param:
             data_sources = DataSource.objects.filter(pk=dataSourceId_param)
         else:
             data_sources = DataSource.objects.all()
-        data_sources, pagination_data = paginate_queryset(
-            data_sources, request)
+            
+        data_sources, pagination_data = paginate_queryset(data_sources, request)
         serialized_data_sources = []
         for data_source in data_sources:
-            try:
-                data_disclosure_agreements = DataDisclosureAgreement.objects.filter(
-                    dataSourceId=data_source, status='listed')
-            except DataDisclosureAgreement.DoesNotExist:
-                data_disclosure_agreements = None
+
+            data_disclosure_agreements_template_ids = (
+                DataDisclosureAgreement.list_unique_dda_template_ids_for_a_data_source(
+                    data_source_id=data_source.id
+                )
+            )
+            ddas = []
+            for dda_template_id in data_disclosure_agreements_template_ids:
+                dda_for_template_id = DataDisclosureAgreement.read_latest_dda_by_template_id_and_data_source_id(
+                    template_id=dda_template_id,
+                    data_source_id=data_source.id,
+                )
+
+                data_disclosure_agreement_serializer = (
+                    DataDisclosureAgreementsSerializer(dda_for_template_id)
+                )
+                ddas.append(
+                    data_disclosure_agreement_serializer.data[
+                        "dataDisclosureAgreementRecord"
+                    ]
+                )
 
             try:
-                verification = Verification.objects.get(
-                    dataSourceId=data_source)
+                verification = Verification.objects.get(dataSourceId=data_source)
                 verification_serializer = VerificationSerializer(verification)
                 verification_data = verification_serializer.data
             except Verification.DoesNotExist:
                 verification_data = {
-                    'id': '',
-                    'dataSourceId': '',
-                    'presentationExchangeId': '',
-                    'presentationState': '',
-                    'presentationRecord': {},
+                    "id": "",
+                    "dataSourceId": "",
+                    "presentationExchangeId": "",
+                    "presentationState": "",
+                    "presentationRecord": {},
                 }
-
-            data_disclosure_agreement_serializer = DataDisclosureAgreementsSerializer(
-                data_disclosure_agreements, many=True)
 
             datasource_serializer = DataSourceSerializer(data_source)
 
             api = [data_source.openApiUrl]
             serialized_data_source = {
-                'dataDisclosureAgreements': data_disclosure_agreement_serializer.data,
-                'api': api,
-                'dataSource': datasource_serializer.data,
-                'verification': verification_data
+                "dataDisclosureAgreements": ddas,
+                "api": api,
+                "dataSource": datasource_serializer.data,
+                "verification": verification_data,
             }
             # Append the serialized data source to the list
             serialized_data_sources.append(serialized_data_source)
 
         # Create the response data dictionary
         response_data = {
-            'dataSources': serialized_data_sources,
-            'pagination': pagination_data
+            "dataSources": serialized_data_sources,
+            "pagination": pagination_data,
         }
 
         # Return the JSON response
