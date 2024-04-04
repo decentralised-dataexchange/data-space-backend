eb701b35514f393be0826b4430dade7f4c0bfb7d
Albin Antony <albin.pa3@gmail.com>
2024-04-04 18:08:51 +0530

    Fix: #41 Super admin login should not be allowed using onboarding login endpoint

--- onboard/views.py
+++ onboard/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .permissions import IsOwnerOrReadOnly
+from onboard.permissions import IsOwnerOrReadOnly
 from .serializers import (DataspaceUserSerializer,
                           RegisterDataspaceUserSerializer)
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
@@ -25,3 +28,16 @@ class UserDetail(APIView):
     def get(self, request):
         serializer = self.serializer_class(request.user, many=False)
         return Response(serializer.data)
+    
+
+class UserLogin(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        if request.data.get('email') and request.data.get('password'):
+            User = get_user_model()
+            user_email = request.data.get('email')
+            user = User.objects.filter(email=user_email).first()
+            if user and user.is_staff:
+                return Response({"Error": "Admin users are not allowed to login"}, status=status.HTTP_403_FORBIDDEN)
+        return super().post(request, *args, **kwargs)
+    
