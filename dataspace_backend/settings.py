31166fff425eb9177b0b188387e62d843047ad8d
George J Padayatti <george.padayatti@igrant.io>
2024-04-05 12:47:25 +0530

    Fix #49: Verification issues
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- dataspace_backend/settings.py
+++ dataspace_backend/settings.py
@@ -172,5 +172,5 @@ if os.environ.get("ENV") == "prod":
 
 STATIC_ROOT = "staticfiles"
 
-DATA_MARKETPLACE_DW_URL = "https://cloudagent.igrant.io/v1/6609f0860c7d8c0001aea929/admin"
-DATA_MARKETPLACE_APIKEY = "ApiKey REDACTED_JWT_TOKEN_1"
+DATA_MARKETPLACE_DW_URL = os.environ.get("DATA_MARKETPLACE_DW_URL", "https://cloudagent.igrant.io/v1/6609f0860c7d8c0001aea929/admin")
+DATA_MARKETPLACE_APIKEY = os.environ.get("DATA_MARKETPLACE_APIKEY", "ApiKey REDACTED_JWT_TOKEN_1")
