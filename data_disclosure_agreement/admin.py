ac98954e05db1e8c230b47177ca5c47f0434afbd
George J Padayatti <george.padayatti@igrant.io>
2024-04-09 23:21:43 +0530

    Fix: Custom django admin fields for DDA table
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- data_disclosure_agreement/admin.py
+++ data_disclosure_agreement/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import DataDisclosureAgreement
 from customadminsite.admin import myadminsite
 
-# Register your models here.
-myadminsite.register(DataDisclosureAgreement)
+
+class DataDisclosureAgreementAdmin(admin.ModelAdmin):
+    list_display = ('templateId', 'status', 'createdAt', 'isLatestVersion')
+
+    def get_list_display(self, request):
+        return self.list_display
+
+
+myadminsite.register(DataDisclosureAgreement, DataDisclosureAgreementAdmin)
