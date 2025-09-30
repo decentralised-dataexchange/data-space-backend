90a5ad1779373b7b5d0bbfc82a163bfb06352d6e
Albin Antony <albin.pa3@gmail.com>
2025-09-30 14:32:29 +0530

    Fix #135: Support name, location, privacy_dashboard_url, industry_sector in software statement

--- organisation/serializers.py
+++ organisation/serializers.py
@@ -1,35 +1,67 @@
 from rest_framework import serializers
-from .models import Organisation, OrganisationIdentity, OrganisationIdentityTemplate, Sector, CodeOfConduct
+from .models import (
+    Organisation,
+    OrganisationIdentity,
+    OrganisationIdentityTemplate,
+    Sector,
+    CodeOfConduct,
+)
+
 
 class OrganisationSerializer(serializers.ModelSerializer):
-    verificationRequestURLPrefix = serializers.CharField(source='owsBaseUrl', read_only=True)
+    verificationRequestURLPrefix = serializers.CharField(
+        source="owsBaseUrl", read_only=True
+    )
+
     class Meta:
         model = Organisation
         fields = [
-            'id', 'coverImageUrl', 'logoUrl', 'name', 'sector', 'location',
-            'policyUrl', 'description', 'verificationRequestURLPrefix', 'openApiUrl', 'credentialOfferEndpoint', 'accessPointEndpoint', 'codeOfConduct'
+            "id",
+            "coverImageUrl",
+            "logoUrl",
+            "name",
+            "sector",
+            "location",
+            "policyUrl",
+            "description",
+            "verificationRequestURLPrefix",
+            "openApiUrl",
+            "credentialOfferEndpoint",
+            "accessPointEndpoint",
+            "codeOfConduct",
+            "privacyDashboardUrl",
         ]
-        read_only_fields = ['id']
+        read_only_fields = ["id"]
+
 
 class OrganisationIdentitySerializer(serializers.ModelSerializer):
     class Meta:
         model = OrganisationIdentity
-        fields = ['id', 'organisationId', 'presentationExchangeId',
-                  'presentationState','isPresentationVerified', 'presentationRecord']
-        
+        fields = [
+            "id",
+            "organisationId",
+            "presentationExchangeId",
+            "presentationState",
+            "isPresentationVerified",
+            "presentationRecord",
+        ]
+
+
 class OrganisationIdentityTemplateSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrganisationIdentityTemplate
         fields = "__all__"
 
+
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sector
-        fields = ['id', 'sectorName']
-        read_only_fields = ['id']
+        fields = ["id", "sectorName"]
+        read_only_fields = ["id"]
+
 
 class CodeOfConductSerializer(serializers.ModelSerializer):
     class Meta:
         model = CodeOfConduct
-        fields = ['id', 'pdfFile', 'createdAt', 'updatedAt']
-        read_only_fields = ['id', 'createdAt', 'updatedAt']
+        fields = ["id", "pdfFile", "createdAt", "updatedAt"]
+        read_only_fields = ["id", "createdAt", "updatedAt"]
