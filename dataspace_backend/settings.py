40015ad95efaae8fec686558fd24a2a4d1d4dff9
Mylavarapu Sarath Sai <sai@igrant.io>
2025-11-21 18:38:26 +0530

    Fix #97: API endpoints should accept both with and without trailing slash
    Signed-off-by: Mylavarapu Sarath Sai <sai@igrant.io>

--- dataspace_backend/settings.py
+++ dataspace_backend/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "dataspace_backend.middleware.NonGetAppendSlashMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
